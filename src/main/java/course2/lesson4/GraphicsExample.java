371c1dfcc34d13b2597ae3401837fbfefe036e1e
ko55ur <viktorsmekalov@inbox.ru>
2022-05-19 14:38:57 +0300

    Lesson 4

---
+++ src/main/java/course2/lesson4/GraphicsExample.java
@@ -0,0 +1,11 @@
+package course2.lesson4;
+
+
+public class GraphicsExample {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ src/main/java/course2/lesson4/javafx/Launcher.java
@@ -0,0 +1,8 @@
+package course2.lesson4.javafx;
+
+
+public class Launcher {
+    public static void main(String[] args) {
+        JavaFxExampleApplication.run(args);
+    }
+}
