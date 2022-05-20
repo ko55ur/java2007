06532b0d6909665f71d57aa1742861df55edfc9d
ko55ur <viktorsmekalov@inbox.ru>
2022-05-20 17:17:46 +0300

    chat update

--- src/main/java/course2/lesson4/chat/client/ChatController.java
+++ src/main/java/course2/lesson4/chat/client/ChatController.java
@@ -1,19 +1,20 @@
 package course2.lesson4.chat.client;
 
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
+import java.awt.event.KeyListener;
 import java.net.URL;
 import java.util.List;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class ChatController implements Initializable {
@@ -46,13 +47,31 @@ public class ChatController implements Initializable {
         if (text == null || text.isBlank()) {
             return;
         }
-        chatArea.appendText(text + System.lineSeparator());
+
+       ObservableList<String> recipient = contacts.getSelectionModel().getSelectedItems();
+        if (!recipient.isEmpty()) {
+            chatArea.appendText(recipient + text + System.lineSeparator());
+            inputField.clear();
+            return;
+
+        } else {
+            assert recipient != null;
+            if (recipient.isEmpty()) {
+
+                chatArea.appendText("broadcast: " + text + System.lineSeparator());
+                inputField.clear();
+                return;
+            }
+        }
+
+        chatArea.requestFocus();
         inputField.clear();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         List<String> names = List.of("Vasya", "Masha", "Petya", "Valera", "Nastya");
-        contacts.setItems(FXCollections.observableList(names));
+        contacts.setItems(FXCollections.observableArrayList(names));
+
     }
 }
